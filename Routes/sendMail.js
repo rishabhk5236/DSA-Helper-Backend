d9bd94d497764d2056354e79883b2ef85a9596c8
Rishabh <rishabhk5236@gmail.com>
2026-03-25 16:02:06 +0530

    modified sendMail

--- Routes/sendMail.js
+++ Routes/sendMail.js
@@ -2,46 +2,50 @@ const express = require("express");
 const router = express.Router();
 const nodemailer = require("nodemailer");
 
-router.post('/',async (req,res)=>{
+router.post('/', async (req, res) => {
 
-    let success=false;
+
+    // # this username and password is for sending otp 
+    USERNAME = "helpdsahelper@gmail.com"
+    PASSWORD = "fpaegunbtsryoioh"
+    let success = false;
 
 
 
     // creating message 
-    const msg={
-        from:"helpdsahelper@gmail.com",
-        to:req.body.email,
-        subject:req.body.subject,
-        html:req.body.htmlText,
-       
+    const msg = {
+        from: "helpdsahelper@gmail.com",
+        to: req.body.email,
+        subject: req.body.subject,
+        html: req.body.htmlText,
+
     };
 
 
-    
 
 
 
- nodemailer.createTransport({
-        service:"gmail",
-        auth:{
-            user:process.env.USERNAME,
-            pass:process.env.PASSWORD
+
+    nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: USERNAME,
+            pass: PASSWORD
         },
-        port:465,
-        secure:true,
-        host:'smtp.gmail.com'
+        port: 465,
+        secure: true,
+        host: 'smtp.gmail.com'
     })
-    .sendMail(msg,(err)=>{
-        if(err){
-            return res.json({success,error:err});
-        }else{
-            success=true;
-            return res.json({success,message:"Email Sent"});
-        }
-    })
+        .sendMail(msg, (err) => {
+            if (err) {
+                return res.json({ success, error: err });
+            } else {
+                success = true;
+                return res.json({ success, message: "Email Sent" });
+            }
+        })
 
 
 })
 
-module.exports=router;
+module.exports = router;
