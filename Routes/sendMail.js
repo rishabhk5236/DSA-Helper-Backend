3b4add6001553ddcde0302713204d0b1272ccdcd
Rishabh <rishabhk5236@gmail.com>
2026-03-26 12:20:44 +0530

    Modified sendmail code

--- Routes/sendMail.js
+++ Routes/sendMail.js
@@ -1,41 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const sgMail = require("@sendgrid/mail");
 
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
+    let success = false;
 
+    try {
+        const msg = {
+            to: req.body.email,
+            from: "helpdsahelper@gmail.com", // must be verified in SendGrid
+            subject: req.body.subject,
+            html: req.body.htmlText,
+        };
 
+        await sgMail.send(msg);
 
+        success = true;
+        return res.json({ success, message: "Email Sent" });
 
-
-
-    // using Twilio SendGrid's v3 Node.js Library
-    // https://github.com/sendgrid/sendgrid-nodejs
-    javascript
-    const sgMail = require('@sendgrid/mail')
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-    // sgMail.setDataResidency('eu'); 
-    // uncomment the above line if you are sending mail using a regional EU subuser
-
-    const msg = {
-        to: req.body.email, // Change to your recipient
-        from: 'helpdsahelper@gmail.com', // Change to your verified sender
-        subject: req.body.subject,
-        html: req.body.htmlText,
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ success, error: err.message });
     }
-    sgMail
-        .send(msg, (err) => {
-            if (err) {
-                return res.json({ success, error: err });
-            } else {
-                success = true;
-                return res.json({ success, message: "Email Sent" });
-            }
-        })
-
-
-})
-
+});
 
 module.exports = router;
-
