c9b25a1b63f1339ba385485d471cb15458aad7d8
Rishabh <rishabhk5236@gmail.com>
2026-07-30 18:28:29 +0530

    email process removed

--- Routes/sendMail.js
+++ Routes/sendMail.js
@@ -1,29 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const sgMail = require("@sendgrid/mail");
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+
 
 router.post("/", async (req, res) => {
-    let success = false;
-
-    try {
-        const msg = {
-            to: req.body.email,
-            from: "helpdsahelper@gmail.com", // must be verified in SendGrid
-            subject: req.body.subject,
-            html: req.body.htmlText,
-        };
-
-        await sgMail.send(msg);
-
-        success = true;
-        return res.json({ success, message: "Email Sent" });
-
-    } catch (err) {
-        console.error(err);
-        return res.status(500).json({ success, error: err.message });
-    }
+    let success = true;
+    return res.json({success,message : "1. I've initially used Nodemailer to send otp to email but nodemailer do not work on render now, 2. Then i've used sendgrid to send mail but after a time it's free subscription got over ,3. i tried resend to send mails but it do not worked for gmail ids it needs domain "})
 });
 
 module.exports = router;
